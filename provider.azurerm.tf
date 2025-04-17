c91c5dd72ceb15fc13eac5590642ef3ba858048e
Ahmad Obay <ahmad.obay@gmail.com>
2025-04-17 15:01:20 -0700

    Initial Import

---
+++ provider.azurerm.tf
@@ -0,0 +1,4 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "abd5b4fd-a8fd-40db-8c20-25af66c54e0c"
+}
---
+++ resource.azurerm_resource_group.rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "code-to-cloud-rg"
+  location = "West US 2"
+}
