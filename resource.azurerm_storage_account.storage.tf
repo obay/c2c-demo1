f8760d78b51d7a0698db9f5e8ebf47ac41cd9f8b
Ahmad Obay <ahmad.obay@gmail.com>
2025-04-23 10:11:40 -0700

    Change storage account replication type from LRS to GRS in Terraform configuration

--- resource.azurerm_storage_account.storage.tf
+++ resource.azurerm_storage_account.storage.tf
@@ -3,5 +3,5 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 }
