0984b4e3cd66a78b5ae707afa6e253859cf14337
Ahmad Obay <ahmad.obay@gmail.com>
2025-04-23 10:47:20 -0700

    Add Azure Monitor Action Group and VM Recommended Alerts module

---
+++ resource.azurerm_monitor_action_group.goa-action-group.tf
@@ -0,0 +1,5 @@
+resource "azurerm_monitor_action_group" "goa-action-group" {
+  name                = "goa-action-group"
+  resource_group_name = azurerm_resource_group.rg.name
+  short_name          = "goa-action-group"
+}
