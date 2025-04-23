1b1902014cbf6ecdaabac3dd76e15fe03771d195
Ahmad Obay <ahmad.obay@gmail.com>
2025-04-23 10:25:51 -0700

    Add Azure backend configuration for Terraform state management

--- terraform.tf
+++ terraform.tf
@@ -4,4 +4,11 @@ terraform {
       source = "hashicorp/azurerm"
     }
   }
+
+    backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstateobaysa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
