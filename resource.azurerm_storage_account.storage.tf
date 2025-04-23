1ec4a877e517cb4f8a7b78c349870d08f2273474
Ahmad Obay <ahmad.obay@gmail.com>
2025-04-23 10:00:49 -0700

    Update storage account name in Terraform configuration

--- resource.azurerm_storage_account.storage.tf
+++ resource.azurerm_storage_account.storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage" {
-  name                     = "c2cobaysadmo123"
+  name                     = "c2cobaysadmo1234"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
