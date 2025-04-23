300f48ca4c4e4b02b71fcbce2e7db32b2aa8d0d7
Ahmad Obay <ahmad.obay@gmail.com>
2025-04-23 10:27:47 -0700

    Fix indentation for Azure backend configuration in Terraform file

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,7 @@ terraform {
     }
   }
 
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstateobaysa"
     container_name       = "tfstate"
